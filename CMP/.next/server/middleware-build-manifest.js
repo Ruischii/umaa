39786cde75617c40d9f0544439d7a9b485f39169
t2ddy <theoleonard@live.com>
2025-12-12 18:47:46 -0800

    fix: update next ato fix security vulnerability

--- CMP/.next/server/middleware-build-manifest.js
+++ CMP/.next/server/middleware-build-manifest.js
@@ -1,22 +1,21 @@
 globalThis.__BUILD_MANIFEST = {
-  "polyfillFiles": [
-    "static/chunks/polyfills.js"
-  ],
-  "devFiles": [],
-  "ampDevFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/webpack.js",
-    "static/chunks/main-app.js"
-  ],
-  "rootMainFilesTree": {},
   "pages": {
     "/_app": []
   },
-  "ampFirstPages": []
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/d833f378d1472378.js",
+    "static/chunks/c2702641a8296e12.js",
+    "static/chunks/18e1305ed299bc68.js",
+    "static/chunks/96c49b58c20030e7.js",
+    "static/chunks/turbopack-aec4c45a82b1dd64.js"
+  ]
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
-
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
 ];
--- CMP/.next/server/server-reference-manifest.js
+++ CMP/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4xr1MjQ1f1M7y74m2JuxU+SbXRqkxOrecZB0iak3B84=\"\n}"
