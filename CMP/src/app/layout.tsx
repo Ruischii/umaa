d9c7f1bc505c75e388e06b34c35c0ddea9c1bebb
t2ddy <theoleonard@live.com>
2025-12-12 18:01:16 -0800

    feat: add vercel analytics 2

--- CMP/src/app/layout.tsx
+++ CMP/src/app/layout.tsx
@@ -5,6 +5,9 @@ import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
 import "../styles/index.css";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,26 +18,22 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head>
         <link rel="icon" href="/favicon.ico?v=2" type="image/x-icon" />
         <link rel="shortcut icon" href="/favicon.ico?v=2" type="image/x-icon" />
       </head>
-
-      <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
-        <Providers>
-          <Header />
-          {children}
-          <Footer />
-          <ScrollToTop />
-        </Providers>
-      </body>
+    <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
+      <Providers>
+        <Header />
+        {children}
+        <Footer />
+        <ScrollToTop />
+        <Analytics />
+        <SpeedInsights />
+      </Providers>
+    </body>
     </html>
   );
 }
 
 import { Providers } from "./providers";
-
